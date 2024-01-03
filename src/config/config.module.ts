158cb6e4c291ce9a1779718a56398521c0dd654a
namhihi237 <yentth237@gmail.com>
2024-01-03 15:12:26 +0700

    add redis cache

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,9 +1,13 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     NestConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CacheModule.register({
       isGlobal: true,
     }),
   ],
--- src/modules/shorter-link/shorter-link.service.ts
+++ src/modules/shorter-link/shorter-link.service.ts
@@ -1,22 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 
 import { Model } from 'mongoose';
 import { Link } from './link.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class ShorterLinkService {
   constructor(
     @InjectModel(Link.name)
     private linkModel: Model<Link>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async getLink(shortUrl) {
+    const cachedValue = await this.cacheManager.get(shortUrl);
+
+    if (cachedValue) {
+      return cachedValue;
+    }
     const link = await this.linkModel.findOne({ shortUrl });
 
     if (!link) {
       return '/notfound';
     }
+
+    await this.cacheManager.set(shortUrl, link.originalUrl, 0); // set ttl 0, because default 5s
 
     return link.originalUrl;
   }
